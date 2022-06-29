7b32645b312fa9a9c914833a5ca4e3a98dc56791
alalude <adesina.lalude@gmail.com>
2022-06-29 15:35:02 -0700

    Spring Boot Files

---
+++ src/test/java/com/spring_git/api_example/ApiExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_git.api_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
